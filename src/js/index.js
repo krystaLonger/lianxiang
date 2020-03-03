bb8a3d3fce44f3d43aa32ec98d1c1d749fa2a112
Phycho <phycho_ljx@163.com>
2020-03-04 01:43:44 +0800

    第三次上传 首页

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,69 @@
 getList()
 function getList(){
 	$.ajax({
-		url:'../lib/Lenovo.json',
+		url:'../lib/ThinkPad.json',
 		dataType:'json',
 		success:function(res){
 			let str = ''
-			res.forEach(item =>{
-				str +=`
-				<li class="clearbox">
-					<p>${item.title}</p>
-					<ol >
+			res.forEach(item => {
+				str += `
+				<div>
+					<a href="">
+						<p>${item.title}</p>
+						<span></span>
+					</a>
+					<ul>
 					`
-					item.list.forEach(item2 => {
-						str += `<li>${item2.name}</li>`
+					item.list.forEach(item =>{
+						str += `
+						<li class="clearbox">
+							<p>${item.name}</p>
+							<ol >
+							`
+							item.type.forEach(item => {
+								str += `<li>${item.word}</li>`
+							})
+							str +=
+							`
+							</ol>
+						</li>
+						`
 					})
-					
 					str +=
-					`	
-					</ol>
-				</li>
+					`
+					</ul>
+					<div class="png">
+						<a href=""><img src="${item.img_src}" ></a>
+						<a href=""><img src="${item.img_url}" ></a>
+					</div>
+				</div>
 				`
 			})
-			$('.list>.option>div>ul').html(str)
+			
+			$('.list>.option')
+			.html(str)
+			.children()
+			.on({
+				mouseenter: function(){
+					$(this).children().stop().show()
+				},
+				mouseleave:() => $('.list>.option ul,.list>.option .png').stop().hide()
+			})
+			
 		}
 	})
 }
+//回到顶部
+ $(window).scroll(() => {
+      if ($(window).scrollTop() >= 200) {
+        $('.sort').fadeIn()
+      } else {
+        $('.sort').fadeOut()
+      }
+    })
+
+    $('.sort .back').click(() => {
+      $('html').animate({
+        scrollTop: 0
+      }, 1000)
+    })
